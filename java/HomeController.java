f49b9f8e8a1826c1c429c11936039e40ef60bc60
Gokulk494 <130535132+Gokulk494@users.noreply.github.com>
2024-03-25 19:35:22 +0530

    Update HomeController.java

--- java/HomeController.java
+++ java/HomeController.java
@@ -1,1 +1,84 @@
+package g.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import g.entity.User;
+import g.repository.UserRepo;
+import g.service.UserService;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@ModelAttribute  //data mapping between the client request and the servers model object 
+	public void commonUser(Principal p, Model m) {
+		if (p != null) {
+			String email = p.getName();
+			User user = userRepo.findByEmail(email);
+			m.addAttribute("user", user);
+		}
+
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+
+	@GetMapping("/signin")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/user/profile")
+	public String profile(Principal p, Model m) {
+		String email = p.getName();
+		User user = userRepo.findByEmail(email);
+		m.addAttribute("user", user);
+		return "profile";
+	}
+
+	@GetMapping("/user/home")
+	public String home() {
+		return "home";
+	}
+
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute User user, HttpSession session, Model m) {
+
+		// System.out.println(user);
+
+		User u = userService.saveUser(user);
+
+		if (u != null) {
+			// System.out.println("save sucess");
+			session.setAttribute("msg", "Register successfully");
+
+		} else {
+			// System.out.println("error in server");
+			session.setAttribute("msg", "Something wrong server");
+		}
+		return "redirect:/register";
+	}
+
+}
